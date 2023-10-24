eef9cd92715b371c4c491a3146f82cde450eb33a
Louie Alfonso <lfriasalfonso@gmail.com>
2023-10-24 23:16:29 +0100

    nextjs roofer project
    nextjs roofer project

--- app/aboutus/page.tsx
+++ app/aboutus/page.tsx
@@ -1,7 +1,23 @@
+import AboutusSection from '@/components/ABoutusSection'
 import React from 'react'
 
 export default function page() {
   return (
-    <div>About us page</div>
+    <>
+    <section className="page__title p_relative">
+      <div className="bg-layer parallax-bg" data-parallax='{"y": 20}' style={{ backgroundImage: "url(images/resource/page-title.png)" }}></div>
+      <div className="auto_container">
+        <div className="content-box p_relative">
+          <h1 className="title">About Us</h1>
+          <ul className="bread-crumb">
+            <li><a href="index.html">Home </a></li>
+            <li><span> - </span>About{" "}
+            </li>
+          </ul>
+        </div>
+      </div>
+    </section>
+    <AboutusSection/>
+    </>
   )
 }
